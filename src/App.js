293dba611114f14b6b3b549c40f79dcc8f4ba543
飛田夏希 <tobitanatsuki@hidanatukinoMBP.elecom>
2022-01-07 13:34:59 +0900

    implement callbacks with useEffect

--- src/App.js
+++ src/App.js
@@ -1,39 +1,37 @@
-import React, { useState } from 'react'
-
-// const App = () => {
-//   const initialStates = {
-//     name: '',
-//     price: 1000
-//   }
-//   const [name, setName] = useState(initialStates.name)
-//   const [price, setPrice] = useState(initialStates.price)
-
-//   return (
-//     <>
-//       <p>現在の{name}は、{price}です。</p>
-//     </>
-//   );
-// }
+import React, { useEffect, useState } from 'react'
 
 const App = props=> {
-  const initialStates = {
-    name: '',
-    price: 1000
-  }
-  const [name, setName] = useState(props.name)
-  const [price, setPrice] = useState(props.price)
-  const reset = () => {
-    setPrice(props.price)
-    setName(props.name)
-  }
+
+  const [state, setState] = useState(props)
+  const reset = () => setState(props)
+  const {name, price} = state
+
+  useEffect(() => { // useEffectはrenderの後に実行されている
+    console.log('useEffect is like componentDidMount or componentDidUpdate')
+  })
+
+  useEffect(() => { // 第二引数に[]を持たせることで、mountされた時のみに実行される関数を実装できる
+  console.log('useEffect is like componentDidMount')
+  }, [])
+
+  useEffect(() => { // 第二引数に[]を持たせることで、mountされた時のみに実行される関数を実装できる
+    console.log('This callback is for name only.')
+  }, [name])
+
+
+  // const renderPreiod = () => {
+  //   console.log('renderPeriod renders perod.')
+  //   return '。'
+  // }
 
   return (
     <>
+      {/* <p>現在の{name}は、{price}です{renderPreiod()}</p> */}
       <p>現在の{name}は、{price}です。</p>
-      <button onClick={() => setPrice(price + 1)}>+1</button>
-      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={() => setState({...state, price: price + 1})}>+1</button>
+      <button onClick={() => setState({...state, price: price - 1})}>-1</button>
       <button onClick={reset}>reset</button>
-      <input value = {name} onChange={e => setName(e.target.value)} />
+      <input value = {name} onChange={e => setState({...state, name: e.target.value})} />
     </>
   );
 }
